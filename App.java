ba413baa73a11f5abd3485dd603df67fd793b32b
ridhoadiwicaksono106 <121468474+ridhoadiwicaksono106@users.noreply.github.com>
2024-01-08 11:46:21 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,11 +1,1 @@
 
-import javax.swing.SwingUtilities;
-
-
-public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-            new TicTacToeGUI();
-        }
-    });
-}
